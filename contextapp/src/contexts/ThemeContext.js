9a73bf048853d84a9f3c4ca1d3329398f91bbe7f
Mike Zahalan <mzahalan@amazon.com>
2025-06-02 14:29:31 -0700

    Lesson 3 - Context Provider
    Created a context provider (ThemeContext)
    Wrapped the components in App.js with with context provided.
    Visually verified in the react debugger that the components are children of the context provider.

---
+++ contextapp/src/contexts/ThemeContext.js
@@ -0,0 +1,28 @@
+import React, {createContext, Component} from 'react'
+
+export const ThemeContext = createContext();
+
+class ThemeContextProvider extends Component {
+    state = { 
+        isLightTheme: true,
+        dark: {
+            text: '#ddd',
+            ui: '#333',
+            bg: '#555'
+        },
+        light: {
+            text: '#555',
+            ui: '#ddd',
+            bg: '#eee'
+        }
+     } 
+    render() { 
+        return (
+            <ThemeContext.Provider value={{...this.state}}>
+                {this.props.children}
+            </ThemeContext.Provider>
+        );
+    }
+}
+ 
+export default ThemeContextProvider;
