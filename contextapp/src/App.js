77915161fb0d15e13c9b6aa7eddcd2bb42b1c40c
Mike Zahalan <MikeZahalan@GMail.COM>
2025-06-03 16:07:02 -0700

    Lesson 7 - Multiple Contexts
    - created a second context
    - added it to the component tree in app.js
    
    Next step will be to consume it in a child component.

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -4,16 +4,18 @@ import BookList from "./components/BookList";
 
 import ThemeContextProvider from "./contexts/ThemeContext";
 import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar/>
-        <BookList/>
-        <ThemeToggle/>
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
-
     </div>
   );
 }
