115303338f2e99060c21b13791d4ecb70a7b7f43
Mike Zahalan <MikeZahalan@GMail.COM>
2025-06-04 17:04:47 -0700

    Lesson 10 - The useState hook
    - Created an array of songs as part of the client side in memory state using useState()

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 const SongList = () => {
+    const [songs, setSongs] = useState([
+        { title: 'Song 1', id: 1},
+        { title: 'Song 2', id: 2},
+        { title: 'Song 3', id: 3}
+    ]);
+    const addSong = () => {
+        setSongs([...songs, {title: 'New Song', id: uuidv4()}]);
+    }
     return ( 
         <div>
             <ul>
-                <li>Song 1</li>
-                <li>Song 2</li>
-                <li>Song 3</li>
+                {songs.map(song => (
+                    <li key={song.id}>{song.title}</li>
+                ))}
             </ul>
+            <button onClick={addSong}>Add Song</button>
         </div>
      );
 }
