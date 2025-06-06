6c1b9603af7804c505530698f50df4460bab4a91
Mike Zahalan <mzahalan@amazon.com>
2025-06-06 16:12:16 -0700

    Lesson 15 - Creating Context with functional components
    Commit Message From AI:
    feat: add BookContext for dynamic book management
    
    - Created new BookContext with useState hook for book data
    - Updated BookList component to consume BookContext
    - Modified App.js to wrap BookList with BookContextProvider

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,15 +1,18 @@
 import {useContext} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
 
 export default function BookList() {
     const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const { books } = useContext(BookContext);
+
     const theme = isLightTheme ? light : dark;
     return (
         <div className="book-list" style={{ color: theme.text, background: theme.bg }}>
             <ul>
-                <li style={{ background: theme.ui }}>The Way of Kings</li>
-                <li style={{ background: theme.ui }}>The Name of the Wind</li>
-                <li style={{ background: theme.ui }}>The Final Empire</li>
+                {books.map(book => (
+                    <li style={{ background: theme.ui }} key={book.key}>{book.title}</li>
+                ))}
             </ul>
         </div>
     )
