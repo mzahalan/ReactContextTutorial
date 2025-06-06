98cb5a1d6fb98e246b1bad3c0fdefe05dc760f20
Mike Zahalan <mzahalan@amazon.com>
2025-06-06 15:47:04 -0700

    Lesson 13 - useContext
    - refactored booklist to useContext, and turned it into a functional component. It's much cleaner looking.

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,22 +1,16 @@
-import React, {Component} from 'react';
+import {useContext} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-    static contextType = ThemeContext;
-
-    render() { 
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
-            <div className="book-list" style={{color: theme.text, background: theme.bg}}>
-                <ul>
-                    <li style={{ background: theme.ui }}>The Way of Kings</li>
-                    <li style={{ background: theme.ui }}>The Name of the Wind</li>
-                    <li style={{ background: theme.ui }}>The Final Empire</li>
-                </ul>
-            </div>
-        )
-    }
+export default function BookList() {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return (
+        <div className="book-list" style={{ color: theme.text, background: theme.bg }}>
+            <ul>
+                <li style={{ background: theme.ui }}>The Way of Kings</li>
+                <li style={{ background: theme.ui }}>The Name of the Wind</li>
+                <li style={{ background: theme.ui }}>The Final Empire</li>
+            </ul>
+        </div>
+    )
 }
- 
-export default BookList;
