b884d03e496ead06be88781d8a6371af3deea2a1
Mike Zahalan <MikeZahalan@GMail.COM>
2025-06-07 14:34:22 -0700

    Lesson 20 - Using Reducers
    Created a reducer to encapsulate the methods of book context. I don't really like it because all we're doing is turning functions into strings that map to methods in a switch statement. It goes against my fiber. But whatever.

--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -1,26 +1,13 @@
-import { createContext, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid';
+import { createContext, useReducer } from 'react'
+import { bookReducer } from '../reducers/BookReducer';
 
 export const BookContext = createContext()
 
 export default function BookContextProvider(props) {
-    const [books, setBooks] = useState([
-        {title: 'The Way of Kings', author: 'Brandon Sanderson', id: 1},
-        {title: 'The Name of the Wind',  author: 'Brandon Sanderson', id: 2},
-        {title: 'The Final Empire',  author: 'Brandon Sanderson', id: 3},
-        {title: 'The Hero of Ages',  author: 'Brandon Sanderson', id: 4}
-    ])
-
-    const addBook = (title, author) => {
-        setBooks([...books, {title, author, id: uuidv4()}])
-    }
-
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id))
-    }
+    const [books, dispatch] = useReducer(bookReducer, [])
 
   return (
-    <BookContext.Provider value={{books, addBook, removeBook}}>
+    <BookContext.Provider value={{books, dispatch}}>
         {props.children}
     </BookContext.Provider>
   )
