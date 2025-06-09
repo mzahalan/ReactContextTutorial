8d8652d19e5008a75423a37a7197270e3fe41ed8
Mike Zahalan <mzahalan@amazon.com>
2025-06-09 09:22:48 -0700

    Lesson 21 - Local Storage
    - Updated the BookList Context to pull from localStorage if it exists. This allows the booklist to persist on the client side even after page refresh.

--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -1,14 +1,24 @@
-import { createContext, useReducer } from 'react'
+import { createContext, useEffect, useReducer } from 'react'
 import { bookReducer } from '../reducers/BookReducer';
 
 export const BookContext = createContext()
 
+const STORAGE_KEY = 'booklist';
+
 export default function BookContextProvider(props) {
-    const [books, dispatch] = useReducer(bookReducer, [])
+
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const bookString = localStorage.getItem(STORAGE_KEY)
+    return bookString ? JSON.parse(bookString) : []
+  })
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(books))
+  }, [books])
 
   return (
-    <BookContext.Provider value={{books, dispatch}}>
-        {props.children}
+    <BookContext.Provider value={{ books, dispatch }}>
+      {props.children}
     </BookContext.Provider>
   )
 }
